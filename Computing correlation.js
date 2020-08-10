409736279c864b0fcb06f9acf643a533fc78b600
BrianNBN <44273986+BrianNBN@users.noreply.github.com>
2020-08-10 16:38:57 +0300

    x

--- Computing correlation.js
+++ Computing correlation.js
@@ -1,9 +1,24 @@
-function phi(table) {
+/*function phi(table) {
     return (table[3] * table[0] - table[2] * table[1]) / 
-    Math.sqrt((table[2] + table[3]) *
-              (table[0] + table[1]) *
-              (table[1] + table[3]) *
-              (table[0] + table[2]));
+      Math.sqrt((table[2] + table[3]) *
+                (table[0] + table[1]) *
+                (table[1] + table[3]) *
+                (table[0] + table[2]));
 }
 
-console.log(phi([76, 9, 4, 1]));
+console.log(phi([76, 9, 4, 1]));*/
+
+/*function phi(table) {
+    return (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt((table[2] + table[3]) *
+    (table[0] + table[1]) *
+    (table[1] + table[3]) *
+    (table[0] + table[2]));
+    }
+    console.log(phi([76, 9, 4, 1]));*/
+
+    console.log(Object.keys({X: 0, Y: 0, Z: 2}));
+
+let objectA = {a: 1, b: 2};
+Object.assign(objectA, {b: 3, c: 4});
+console.log(objectA);
