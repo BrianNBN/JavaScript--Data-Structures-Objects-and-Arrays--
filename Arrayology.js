8128c39101d23d241f455d4ac16598097116a7b6
BrianNBN <44273986+BrianNBN@users.noreply.github.com>
2020-08-10 21:48:32 +0300

    x

---
+++ Arrayology.js
@@ -0,0 +1,22 @@
+let todoList = [];
+function remember(task) {
+    todoList.push(task);
+}
+function getTask() {
+    return todoList.shift();
+}
+function rememberUrgently(task) {
+    todoList.unshift(task);
+}
+
+
+console.log([1, 2, 3, 2, 1].indexOf(2));
+console.log([1, 2, 3, 2, 1].lastIndexOf(2));
+//slice 
+console.log([0, 1, 2, 3, 4].slice(2, 4));
+console.log([0, 1, 2, 3, 4].slice(2));
+
+function remove(array, index) {
+    return array.slice(0, index)
+     .concat
+}
