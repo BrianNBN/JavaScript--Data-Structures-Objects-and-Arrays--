d095b2ad8669b9e0f4c1de77097b63d9ae67d97e
BrianNBN <44273986+BrianNBN@users.noreply.github.com>
2020-08-10 18:28:31 +0300

    x

--- journal.js
+++ journal.js
@@ -1,4 +1,4 @@
- var JOURNAL = [
+var JOURNAL = [
   {"events":["carrot","exercise","weekend"],"squirrel":false},
   {"events":["bread","pudding","brushed teeth","weekend","touched tree"],"squirrel":false},
   {"events":["carrot","nachos","brushed teeth","cycling","weekend"],"squirrel":false},
