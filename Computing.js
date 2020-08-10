7481bf681aa2e17f6bb7d2b8e49c8f5e9f7f69ba
BrianNBN <44273986+BrianNBN@users.noreply.github.com>
2020-08-10 17:45:26 +0300

    c

--- Computing.js
+++ Computing.js
@@ -1,5 +1,5 @@
 
-function phi(table) {
+/*function phi(table) {
     return (table[3] * table[0] - table[2] * table[1]) / 
       Math.sqrt((table[2] + table[3]) *
                 (table[0] + table[1]) *
@@ -7,5 +7,20 @@ function phi(table) {
                 (table[0] + table[2]));
 }
 
-console.log(phi([76, 9, 4, 1]));
+console.log(phi([76, 9, 4, 1]));*/
+
+
+function tableFor (event, journal) {
+    let table = [0, 0, 0, 0];
+    for (let i=0; i < journal.length; i++) {
+        let entry = journal[i], index = 0;
+        if (entry.events,includes(event)) index +=1;
+        if (entry.squirrel) index += 2;
+        table[index] += 1;
+    }
+    return table;
+}
+console.log(tableFor("pizza", journal));
+
+
 
