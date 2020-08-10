f630d5a672fd381afa582b4bac72aac6e59d01e4
BrianNBN <44273986+BrianNBN@users.noreply.github.com>
2020-08-10 18:28:17 +0300

    x

---
+++ Arrayloops.js
@@ -0,0 +1,10 @@
+/*const JOURNAL = require("./journal");
+
+for (let i = 0; i < JOURNAL.length; i++) {
+    let entry = JOURNAL[i];
+    //Do something with entry
+}*/
+
+for (let entry of JOURNAL) {
+    console.log(`${entry.events,length} events.`);
+}
