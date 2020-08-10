3cc5f398553522fb54858c9edd545b5970191f07
BrianNBN <44273986+BrianNBN@users.noreply.github.com>
2020-08-10 11:39:56 +0300

    c

--- Objects.js
+++ Objects.js
@@ -12,9 +12,28 @@ let descriptions = {
     "touched tree": "Touched a tree"
 };*/
 
-let anObject = {left: 1, right: 2};
+/*let anObject = {left: 1, right: 2};
 console.log(anObject.left);
 delete anObject.left;
 console.log(anObject.left);
 console.log("left" in anObject);
-console.log("right" in anObject);
+console.log("right" in anObject);*/
+
+console.log(Object.keys({X: 0, Y: 0, Z: 2}));
+
+let objectA = {a: 1, b: 2};
+Object.assign(objectA, {b: 3, c: 4});
+console.log(objectA);
+
+let journal = [
+    {events: ["work", "touched tree", "pizza",
+    "running", "television"],
+    squirrel: false},
+    {events: ["work", "ice cream", "cauliflower",
+    "lasagna", "touched tree", "brushed teeth"],
+    squirrel: false},
+    {events: ["weekend", "cycling", "break", "peanuts",
+    "beer"],
+    squirrel: true},
+    /* and so on... */
+    ];
